4a4e86f8eafb0b6acedd9ee24f09a4dcb43b98fa
Marksoloff <marksoloffwrites@gmail.com>
2018-04-03 20:20:08 -0500

    Q&A strings, no stats

--- phrases.py
+++ phrases.py
@@ -5,18 +5,8 @@ Created on Tue Apr  3 19:58:32 2018
 @author: marks
 """
 
-personalities = ("Rookie", "Washed up", "Retired", "Unhinged", "Slick", "Incompetent")
-bear_types = ("Grizzly Bear", "Polar Bear", "Panda", "Black Bear", "Sun Bear", "Honey Badger")
-criminal_roles = ("Enforcer", "Mastermind", "Getaway Driver", "Hacker", "Thief", "Face")
+"Badda-BOOM! Hey get a load of you, ya stinkin':"
 
-boy_name = ("Bugsy", "Artie", "Mooksie", "Petey", "Vinnie", "Lou", "Mungo", "Mack", "Tony", "Teddy", "Bobo", "Chucky", "Bobby", "Lucio", "Musky", "Sticks", "Clawsy", "Yogi")
-girl_name = ("Ursula", "Coco", "Wendy", "Velvet", "Betty", "Mama", "Louisa", "Belladonna", "Carmen", "Jaimie", "Veronica", "Squeaky", "Bang-Bang", "Cookies", "Delores")
-asex_name = ("Grumbles", "Pork-Bone", "Pittsburgh", "Brumbo", "Binky", "Stabs", "Punchy", "Husky", "Shortstack", "Pookie", "Bing Bing", "Clawsy", "Dead-eye", "Knuckles", "Mookie", "Sneaky", "Two-toes", "Shades", "Bully", "Meatsy", "Chuckles", "Bingo", "Pathos")    
-surname = ("Malone", "LeRoy", "Falcone", "O'Malley", "Pitsacotta", "Rourke", "Putanesca", "the Chopper", "MacKenzie", "the Knife", "Fujimora", "Tanaka", "the Vermin of Scarsdale")    
-
-
-f"""Badda-BOOM! Hey get a load of you, ya stinkin':
-        
 "Lookin' good, ya filthy animal! Now let's figure out your bear name. Hit ENTER, okay?"
 
 "Would you like to think up your own bear name? Y/N? \n"
@@ -36,14 +26,13 @@ f"""Badda-BOOM! Hey get a load of you, ya stinkin':
 
 "Pleased to meetcha miss. Charmed, I'm sure."
 "Hit ENTER again and I'll cook up a real crackerjack name for ya.\n"
-"\nAnd your name is: "+ name("F") + "! \nWelcome to the gang!" 
+"\nAnd your name is: "+ name("F") + "! \nWelcome to the gang!"
 
  "\nWhat's that? Stop bein' a mook and type 'M' or 'F'!"
- 
+
  "Ey, I been wonderin'... Does your bear have a gender?: Y/N?"
  "\nI don't understand you, kid. Just type 'Y' or 'N' already!"
 
 "Welcome to da Honey Heist Bearacter Generator"
 "Press ENTER to create a new character, capeesh?"
 "\nMazel tov, kid."
-    
